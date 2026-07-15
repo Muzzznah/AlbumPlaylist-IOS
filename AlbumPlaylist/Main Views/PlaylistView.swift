61e0560ce203540b401fec2bd676fb3e87aa2c7d
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-15 18:04:36 -0400

    Added placeholder PlaylistView for second tab

---
+++ AlbumPlaylist/Main Views/PlaylistView.swift
@@ -0,0 +1,19 @@
+//
+//  PlaylistView.swift
+//  AlbumPlaylist
+//
+//  Created by Muznah Adil on 2026-07-15.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        Text("Playlist coming soon")
+    }
+}
+
+#Preview {
+    PlaylistView()
+}

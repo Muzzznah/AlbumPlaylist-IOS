6c6d67e98937d1ff954896745de97ba13e44c59a
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-15 15:10:01 -0400

    Initial Commit

---
+++ AlbumPlaylist/AlbumPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumPlaylistApp.swift
+//  AlbumPlaylist
+//
+//  Created by Muznah Adil on 2026-07-15.
+//
+
+import SwiftUI
+
+@main
+struct AlbumPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

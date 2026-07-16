82263fdee0ecabc442713ea0627f58d4b020dda5
Muznah Adil <muznahadil.2005@gmail.com>
2026-07-15 20:55:33 -0400

    Added playlist list with swipe to remove

--- AlbumPlaylist/Main Views/PlaylistView.swift
+++ AlbumPlaylist/Main Views/PlaylistView.swift
@@ -9,11 +9,25 @@ import Foundation
 import SwiftUI
 
 struct PlaylistView: View {
-    
+
     var albumStore: AlbumStore
-    
+
     var body: some View {
-        Text("Playlist coming soon")
+        List {
+            ForEach(albumStore.albums, id: \.id) { album in
+                VStack(alignment: .leading) {
+                    // The album name.
+                    Text(album.collectionName)
+                        .font(.headline)
+                    // The artist name.
+                    Text(album.artistName)
+                        .font(.subheadline)
+                        .italic()
+                }
+            }.onDelete { indexSet in
+                albumStore.removeFromPlaylist(atIndexSet: indexSet)
+            }
+        }
     }
 }
 
